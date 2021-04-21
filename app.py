67918e1988da6d7baba23a24b75d304dc820b9dd
Mad-Mange <madheli@hotmail.com>
2021-04-21 10:19:12 +0200

    Changed some code in index.html

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', "POST"])
 def index():
   return render_template('index.html')
 
-  # Didier makes a API here :)
+  # Didier makes a API here
+  # Didier sends mail to client
 
 if __name__ == '__main__':
   port = int(os.environ.get('PORT', 5000))
