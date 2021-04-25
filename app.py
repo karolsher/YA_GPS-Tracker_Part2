486d4db7049a42210edd27a98d5322f0fddf7660
Mad-Mange <madheli@hotmail.com>
2021-04-25 17:28:49 +0200

    Turn off API, try to find error on index.html page

--- app.py
+++ app.py
@@ -8,12 +8,12 @@ def index():
   return render_template('index.html')
 
 # Didier makes a API here
-@app.route('/api/v1/firmware', methods=['GET'])
-def api_all():
-  SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-  json_url = os.path.join(SITE_ROOT, "static/js", "data.json")
-  data = json.load(open(json_url))
-  return jsonify(data)
+# @app.route('/api/v1/firmware', methods=['GET'])
+# def api_all():
+#   SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+#   json_url = os.path.join(SITE_ROOT, "static/js", "data.json")
+#   data = json.load(open(json_url))
+#   return jsonify(data)
 
 # Didier sends mail to client
 
