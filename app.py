15a2dee55c239c9c7d1fdbd89ea5fc493d2f5601
Mad-Mange <madheli@hotmail.com>
2021-04-25 17:08:20 +0200

    Changed index.html page

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ def index():
   return render_template('index.html')
 
   # Didier makes a API here
-
 @app.route('/api/v1/firmware', methods=['GET'])
 def api_all():
   SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
