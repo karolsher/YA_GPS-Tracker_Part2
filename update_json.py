89f0f92746785be222a7558cf9f29003a79aeac6
Mad-Mange <madheli@hotmail.com>
2021-04-27 06:56:27 +0200

    Test autoUpdate json

--- update_json.py
+++ update_json.py
@@ -1,1 +1,16 @@
+import json
+
+firmware = '1.000'
+unittest = '200'
+integrationtest = '300'
+performancetest : '00'
+
+with open("static/js/data.json", "r") as write_json:
+    data = json.load(write_json)
+    for p in data:
+        p['unittest'] = unittest
+        p['integrationtest'] = integrationtest
+with open("static/js/data.json", "w") as write_json:
+    json.dump(data, write_json)
+
 print("Updating Json with new values...")
